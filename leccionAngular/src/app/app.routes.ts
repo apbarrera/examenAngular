24264831464834a8f4b0a235063462af22d607c2
Arianna Barrera <159987465+apbarrera@users.noreply.github.com>
2025-02-18 10:50:06 -0500

    Delete leccionAngular directory

--- leccionAngular/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { CartasComponent } from './shared/cartas/cartas.component';
-
-export const routes: Routes = [
-  { path: 'recursos', component: CartasComponent },
-  { path: '', redirectTo: '/recursos', pathMatch: 'full' }
-];
